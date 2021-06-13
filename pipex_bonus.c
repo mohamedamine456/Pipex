30a1eb53ded43e7170bff1e5f13fdf758fcf3660
Mohamed amine Lachheb <aminelachheb456@gmail.com>
2021-06-13 20:45:10 +0100

    bonus was incorrect

--- pipex_bonus.c
+++ pipex_bonus.c
@@ -6,7 +6,7 @@
 /*   By: mlachheb <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/12 18:51:01 by mlachheb          #+#    #+#             */
-/*   Updated: 2021/06/13 16:18:32 by mlachheb         ###   ########.fr       */
+/*   Updated: 2021/06/13 20:44:45 by mlachheb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 int	main(int argc, char **argv, char **envp)
 {
 	t_command	*command;
+	int			in_file;
+	int			out_file;
 
 	if (argc < 5)
 	{
@@ -24,8 +26,10 @@ int	main(int argc, char **argv, char **envp)
 		return (1);
 	}
 	command = get_data(argv);
+	in_file = open_file(argv[1], 0);
+	out_file = open_file(argv[argc -1], 1);
 	replace_commands(&command, envp);
-	exec_pipes(command, envp, argv[1], argv[argc - 1]);
+	exec_pipes(command, envp, in_file, out_file);
 	ft_free_command(&command);
 	return (0);
 }
